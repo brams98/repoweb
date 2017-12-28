13ef1dad5045fdbfe89ad06120a4a285d6c28494
unknown <rahman.sumirat@binus.ac.id>
2017-12-28 14:18:16 +0700

    update tambah user allowance di book php

--- book.php
+++ book.php
@@ -1,5 +1,8 @@
 <?php 
 	
+header("Access-Control-Allow-Origin: *");
+
+
 	$book_list = [];
 	$json = json_decode(file_get_contents("http://mcc-ws-odd1718.herokuapp.com/books.php"));
 	$book_list = $json;
